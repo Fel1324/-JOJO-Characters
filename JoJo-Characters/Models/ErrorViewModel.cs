655fb7480849cd638dcb2221fd96bd4a9c49f806
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2023-03-06 08:52:42 -0300

    Projeto criado

---
+++ JoJo-Characters/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JoJo_Characters.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
